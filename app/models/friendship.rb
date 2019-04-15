df43edd0a9f53809eb112e811cca755acd6c47e8
mohammedalnaggar <mohammed.alnaggar93@gmail.com>
2019-04-16 01:21:23 +0200

    modifying views of notifications

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,12 +2,16 @@ class Friendship < ApplicationRecord
     belongs_to :user
     belongs_to :friend, :class_name => 'User'
 
-    after_commit :follow, on: :create
-    
+    after_commit :friended, on: :create 
+    after_commit :unfriended, on: :destroy
+
     private
 
-    def follow
+    def friended
         Notification.create(notify_type: 'follow', actor: self.user, user: self.friend, target: self.friend)
     end
     
+    def unfriended
+        Notification.create(notify_type: 'unfriend', actor: self.user, user: self.friend, target: self.friend)
+    end
 end
