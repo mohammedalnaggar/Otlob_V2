ae86d0eddfd32bf56799a263be51f4af5e405f44
mohammedalnaggar <mohammed.alnaggar93@gmail.com>
2019-04-17 00:18:12 +0200

    adjusting orders page

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   has_one_attached :menu
   enum status: [ :waiting, :finished]
-  enum order_for: [ :breakfast, :launch, :dinner]
+  enum order_for: [ :Breakfast, :Launch, :Dinner]
 end
