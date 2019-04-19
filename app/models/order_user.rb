3302c98fb8ceb43d75632c01fe45369747288bd9
mohammedalnaggar <mohammed.alnaggar93@gmail.com>
2019-04-19 14:50:06 +0200

    editing minor features - notifications - order-member remove

--- app/models/order_user.rb
+++ app/models/order_user.rb
@@ -2,17 +2,19 @@ class OrderUser < ApplicationRecord
   belongs_to :order
   belongs_to :user
   has_many :order_details, dependent: :destroy
-  enum status: [ :pinding, :joined]
+  enum status: [ :pending, :joined]
   
   after_commit :create_notifications, on: [:create]
   
   def create_notifications
     for user in self.order.users 
-      Notification.create(notify_type: 'invitation',
-        actor: self.user,
-        user: user,
-        target: user,
-        second_target: self.order)
+      if user != self.order.user
+        Notification.create(notify_type: 'invitation',
+          actor: self.order.user,
+          user: user,
+          target: user,
+          second_target: self.order)
+        end
       end
   end
 end
