3bc82bde6a1bb6dd7bb6c89cc8bd32f30c04b5d7
bassant sayed <bassantsayed19@gmail.com>
2019-04-20 00:29:04 +0200

    Creating Home page friends activities

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     if current_user
-    @latestOrders = current_user.orders.last("10")
+    @latestOrders = current_user.orders.order(created_at: :desc).last("10")
+    @friendsOrders = current_user.friends
     end
 
   end
